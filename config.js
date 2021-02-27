5e7412da0446a5e0740ae941f8d06640550b231e
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-02-26 23:14:16 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
  dirBase : './database/base_1.db',
- superusers: [""], //SAHİP İD
- token: '',
+ superusers: ["790265200770809857"], //SAHİP İD
+ token: 'ODA2MjI2NTE0MjU2NDYxODY0.YBmW8w.xtnBy-zNDGWrxUMlrH6XFs7uAgw',
  ytKey: '',//APİ KEY
- prefix: '', //prefix
+ prefix: '>', //prefix
  statusBOT: '🔥YouMusic|🔥!help|🔥!stats',
  categories: [
   {name: "general", priority: 5},
